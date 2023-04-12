5da48e829b25d1f51c13c20cb6c75d28c52b0875
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-12 11:27:12 -0500

    created html page and css for styling

---
+++ main.js
@@ -0,0 +1,8 @@
+let AddToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+AddToDoButton.addEventListener('click', function (){
+    var paragraph = document.createElement('p')
+    toDoContainer
+})
